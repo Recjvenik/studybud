d7a1786e4a1b21094c7c9a219f47ed8fba5bc611
Recjvenik <himanshum9600@gmail.comm>
2022-04-15 21:05:43 +0530

    make it better

--- base/urls.py
+++ base/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
 
-    path('', views.home, name="home"),
+    path('/', views.home, name="home"),
     path('room/<str:pk>/', views.room, name="room"),
     path('profile/<int:pk>/', views.userProfile, name="user-profile"),
     
